34d92250d5eec322b541db713d5f69e1b709bb69
Armando Maldonado Conejo <armand1live@gmail.com>
2015-06-27 23:24:29 -0500

    create project

---
+++ js/panel.js
@@ -0,0 +1,52 @@
+/**
+ * The control panel.
+ */
+var Panel = {
+    init: function () {
+
+        $('.panel').draggable();
+        $('.accordion').accordion({
+            collapsible: false,
+        });
+        $('.option_label').click(function () {
+            $(this).prev().click();
+        });
+        $('#hide_instructions').click(function () {
+            $('#instructions_panel').slideUp();
+        });
+        $('#play_panel').css({
+            top: 5
+        });
+        $('#button2').attr('disabled', 'disabled');
+    },
+    /**
+     * Get the user selected path-finder.
+     * TODO: clean up this messy code.
+     */
+    getFinder: function () {
+        var finder, selected_header, heuristic, allowDiagonal, biDirectional, dontCrossCorners, weight, trackRecursion, timeLimit;
+
+        selected_header = $(
+            '#algorithm_panel ' +
+            '.ui-accordion-header[aria-selected=true]'
+        ).attr('id');
+
+        //    allowDiagonal = typeof $('#breadthfirst_section ' +
+        //                             '.allow_diagonal:checked').val() !== 'undefined';
+        //    biDirectional = typeof $('#breadthfirst_section ' +
+        //                             '.bi-directional:checked').val() !== 'undefined';
+        //    dontCrossCorners = typeof $('#breadthfirst_section ' +
+        //                             '.dont_cross_corners:checked').val() !=='undefined';
+        console.log(allowDiagonal);
+        console.log(biDirectional);
+        console.log(dontCrossCorners);
+
+        finder = new PF.BreadthFirstFinder({
+            allowDiagonal: true,
+            dontCrossCorners: true
+        });
+
+
+        return finder;
+    }
+};
